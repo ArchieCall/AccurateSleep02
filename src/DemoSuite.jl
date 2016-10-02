35069358b88584e66fc8a8f9056bcbf7d6d6f488
ArchieCall <archcall@gmail.com>
2016-10-01 21:39:07 -0400

    ee

--- src/DemoSuite.jl
+++ src/DemoSuite.jl
@@ -11,9 +11,18 @@ function DemoSuite()
   sleep_ns(.0001)
   sleep_ns(.00001)
   sleep_ns(.000003)
+
+  println("\n--- benchmark sleep of .001 secs")
   @show @benchmark sleep(.001)
   @show @benchmark Libc.systemsleep(.001)
   @show @benchmark sleep_ns(.001)
+
+  println("\n--- benchmark sleep of .005 secs")
+  @show @benchmark sleep(.005)
+  @show @benchmark Libc.systemsleep(.005)
+  @show @benchmark sleep_ns(.005)
+
+  println("\nVarious CDF reports")
   AccurateSleep.Demo2(.001, 2, 1000)
   AccurateSleep.Demo2(.0001, 1, 5000)
   AccurateSleep.Demo2(.000003, 1, 5000)
