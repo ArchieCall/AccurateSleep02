8d54a7351d55865c478a95943cb9a298f81f67c3
ArchieCall <archcall@gmail.com>
2016-10-05 04:08:20 -0400

    e

--- src/DemoCPUImpact.jl
+++ src/DemoCPUImpact.jl
@@ -9,6 +9,7 @@ function DemoCPUImpact()
   end
   RunningSecs = 20
   DesiredSleep = .0075
+  const tics_per_sec = 1_000_000_000.
 
   println("DemoCPUImpact v.3002 dated 10-05-2016")
   println(" ")
@@ -16,23 +17,15 @@ function DemoCPUImpact()
   println("... cpuloading_sleep_ns  => continuous calls to sleep_ns Function")
   @printf("... parms:  RunningSecs => %4i secs,  DesiredSleep => %11.9f\n", RunningSecs, DesiredSleep)
   println("=========================================================================================")
-  NumIters = convert(Int, round(RunningSecs / DesiredSleep))
-  const tics_per_sec = 1_000_000_000.
 
+  DesiredSleep = 1.
+  NumIters = convert(Int, round(RunningSecs / DesiredSleep))
   println("")
   println("=========================================================================================")
-  @printf("... check your cpu loading For Function sleep_ns?\n")
+  @printf("... for %i secs check cpu loading using sleep_ns() of %11.9f seconds\n", RunningSecs, DesiredSleep)
   println("=========================================================================================")
   for i in 1:NumIters
     sleep_ns(DesiredSleep)
-  end
-
-  println("")
-  println("=========================================================================================")
-  @printf("... check your cpu loading For Function Libc.systemsleep?\n")
-  println("=========================================================================================")
-  for i in 1:NumIters
-    Libc.systemsleep(DesiredSleep)
   end
 
   println("")
