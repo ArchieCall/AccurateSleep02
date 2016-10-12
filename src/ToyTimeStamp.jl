b142637202cf3947cae50954e96f3edab9559bff
ArchieCall <archcall@gmail.com>
2016-10-12 16:33:15 -0400

    4

--- src/ToyTimeStamp.jl
+++ src/ToyTimeStamp.jl
@@ -1,6 +1,10 @@
-function ToyTimeStamp(NumSecs::Integer)
+function ToyTimeStamp()
   const TicsPerSec = 1_000_000_000
-  const OverheadSleepLoop = .000001300
+  #const OverheadSleepLoop = .000001300
+  const OverheadSleepLoop = .0
+
+  NumIters = 100
+  SleepPerIter = 1./60.
   for k = 1:2
     println("")
     if k == 1
@@ -9,10 +13,10 @@ function ToyTimeStamp(NumSecs::Integer)
       println("ToyTimeStamp for sleep_ns() function")
     end
 
-    DesiredSleep = 1.0
-    SavedTime = Array{Float64}(NumSecs)
+    DesiredSleep = SleepPerIter
+    SavedTime = Array{Float64}(NumIters)
     BeginSecTic = time_ns()  #-- assume this is beginning second
-    for i in 1:NumSecs
+    for i in 1:NumIters
       if k == 1
         Libc.systemsleep(DesiredSleep)
       else
@@ -21,10 +25,10 @@ function ToyTimeStamp(NumSecs::Integer)
 
       EndSecTic = time_ns()
       ElapsedTime = (EndSecTic - BeginSecTic) / TicsPerSec
-      DesiredSleep = (i + 1.) - ElapsedTime - OverheadSleepLoop
+      DesiredSleep = ((i + 1.) * SleepPerIter) - ElapsedTime - OverheadSleepLoop
       SavedTime[i] = ElapsedTime
     end
-    for i in 1:NumSecs
+    for i in 1:NumIters
       @printf("ElapsedTime => %12.9f seconds\n", SavedTime[i])
     end
   end
