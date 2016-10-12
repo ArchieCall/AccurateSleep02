d28bc2199219758f37d6bf88a336de4b1efeff06
ArchieCall <archcall@gmail.com>
2016-10-12 17:30:04 -0400

    44

--- src/ToyTimeStamp.jl
+++ src/ToyTimeStamp.jl
@@ -19,7 +19,7 @@ function ToyTimeStamp()
     BeginSecTic = time_ns()  #-- assume this is beginning second
     for i in 1:NumIters
       if k == 1
-        @printf("DesiredSleep => %12.9f secs \n", DesiredSleep)
+        #@printf("DesiredSleep => %12.9f secs \n", DesiredSleep)
         sleep_ns(DesiredSleep)
         #Libc.systemsleep(DesiredSleep)
       else
