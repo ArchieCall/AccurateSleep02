593ab18192c6de5618b80f0c94f47aa0d7b1efdc
ArchieCall <archcall@gmail.com>
2016-10-15 04:06:46 -0400

    t

--- src/AccurateSleep.jl
+++ src/AccurateSleep.jl
@@ -1,14 +1,14 @@
-#-- 10-14-2016
+#-- 10-15-2016
 module AccurateSleep
 function sleep_ns(sleep_time::AbstractFloat)
-  const burn_time_threshold = .0019   #-- time in seconds that is reserved For burning
   const tics_per_sec = 1_000_000_000  #-- number of tics in one sec
-  const min_systemsleep = .001    #-- Libc.systemsleep min value - (If used)
-  const max_sleep = 86_400_000.   #-- 1000 day maximum
-  const min_sleep = .000001000    #-- 1 microsecond minimum
+  const min_sleep = .000001000        #-- minimum allowed sleep_time (secs)
+  const max_sleep = 86_400_000.       #-- maximum allowed sleep_time (secs)
+  const burn_time_threshold = .0015   #-- time reserved For burning (secs)
+  const min_systemsleep = .001        #-- time below which Libc.systemsleep is not used
   BegTic = time_ns()  #-- get beginning time tic
-  AddedTics0 = round(sleep_time * tics_per_sec)
-  AddedTics = convert(UInt64, AddedTics0)
+  AddedTics0 = round(sleep_time * tics_per_sec)  #-- eliminate fractional tics
+  AddedTics = convert(UInt64, AddedTics0)        #-- convert to UInt64
   EndTic = BegTic + AddedTics  #-- final tic to equal or exceed in busy loop
   #-- validate the value of sleep_time
   if sleep_time < min_sleep
@@ -50,7 +50,6 @@ function sleep_ns(sleep_time::AbstractFloat)
     @printf("Interrupt timer is set too high!    Diff => %12.9f seconds\n", Diff)
     StoppedBecauseInterruptTimerSetTooHigh()
   end
-
   return nothing
 end #-- end of sleep_ns
 export sleep_ns
