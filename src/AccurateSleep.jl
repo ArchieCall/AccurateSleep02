e80b5d6f7b22eec41edd359b4d001b497628d004
ArchieCall <archcall@gmail.com>
2016-10-17 01:46:36 -0400

    j

--- src/AccurateSleep.jl
+++ src/AccurateSleep.jl
@@ -1,76 +1,94 @@
-#-- 10-15-2016
+#-- 10-17-2016
 module AccurateSleep
-function sleep_ns(sleep_time::AbstractFloat)
-  const tics_per_sec = 1_000_000_000  #-- number of tics in one sec
-  const min_sleep = .000001000        #-- minimum allowed sleep_time (secs)
-  const max_sleep = 86_400_000.       #-- maximum allowed sleep_time (secs)
-  const burn_threshold = .0015     #-- time reserved For burning (secs)
-  const min_systemsleep = .0010    #-- time below which Libc.systemsleep has no accuracy
-  const diff_limit = .00004        #-- time below which Libc.systemsleep has no accuracy #const diff_limit = .00010
-  const ShowErrors = true         #-- if error, display error message
+function sleep_ns(SleepTime::AbstractFloat)
+  #-- SleepTime is desired time to accurately sleep in seconds
+  const TicsPerSec = 1_000_000_000   #-- number of tics in one sec
+  const MinSleep = .000001000        #-- minimum allowed SleepTime (secs)
+  const MaxSleep = 1_000_000.        #-- maximum allowed SleepTime (secs)
+  const BurnThreshold = .0013        #-- time reserved For burning (secs)
+  const MinLibcSystemSleep = .0010   #-- time below which Libc.systemsleep has no accuracy (secs)
+  const DiffLimit = .00004           #-- error returned, if Diff above this limit (secs)
+  const ShowErrors = false           #-- display error messages if true (bool)
+  const QuitOnDiffError = false      #-- if true, quit on Diff error (bool)
+  const QuitOnParmError = false      #-- if true, quit on Parm error (bool)
 
-  BegTic = time_ns()  #-- get beginning time tic
-  AddedTics0 = round(sleep_time * tics_per_sec)  #-- eliminate fractional tics
-  AddedTics = convert(UInt64, AddedTics0)        #-- convert to UInt64
-  EndTic = BegTic + AddedTics  #-- final tic to equal or exceed in busy loop
+  BegTic = time_ns()   #-- get beginning time tic
 
-  #-- validate the value of sleep_time
-  if sleep_time < min_sleep
-    @printf("parameter error:  sleep_time => %10.8f is less than %10.8f secs!!\n",
-    sleep_time, min_sleep)
-    println("program halted.")
-    SpecifiedSleepTooLow()
+  #-- validate the value of SleepTime
+  ParmOK = true
+  if SleepTime < MinSleep
+    @printf("Parm Error:  SleepTime = %10.9f is less than allowed min of %10.8f secs!!\n",
+    SleepTime, MinSleep)
+    ParmOK = false
+    #println("... program halted.")
+    #quit()
   end
-  if sleep_time > max_sleep
-    @printf("parameter error:  sleep_time => %12.1f is greater than %10.1f secs!!\n",
-    sleep_time, max_sleep)
-    println("program halted.")
-    SpecifiedSleepTooHigh()
-  end
-
-  #--- calc sleeping time
-  time_for_sleeping = 0.
-  if sleep_time > burn_threshold
-    time_for_sleeping = sleep_time - burn_threshold
+  if SleepTime > MaxSleep
+    @printf("Parm Error:  SleepTime = %12.1f is greater allowed max of %10.1f secs!!\n",
+    SleepTime, MaxSleep)
+    ParmOK = false
+    #println("... program halted.")
+    #quit()
   end
 
-  #-- sleep a fraction of total sleep_time
-  if time_for_sleeping >= min_systemsleep
-    Libc.systemsleep(time_for_sleeping)  #-- sleep a portion of sleep_time
-  end
+  if ParmOK
 
-  #------ burn off remaining time in busy loop
-  CurrTic = time_ns()
-  while true
-    if CurrTic >= EndTic
-      break
+    AddedTics0 = round(SleepTime * TicsPerSec)     #-- eliminate fractional tics
+    AddedTics = convert(UInt64, AddedTics0)        #-- convert to UInt64
+    EndTic = BegTic + AddedTics   #-- final tic to equal or exceed in burn loop
+
+    #--- calc how much time to sleep
+    TimeToSleep = 0.
+    if SleepTime > BurnThreshold
+      TimeToSleep = SleepTime - BurnThreshold
     end
+
+    #-- only sleep if above the level where Libc.systemsleep has accuracy
+    if TimeToSleep >= MinLibcSystemSleep
+      Libc.systemsleep(TimeToSleep)  #-- sleep a portion of SleepTime
+    end
+
+    #------ burn off remaining time in a loop
     CurrTic = time_ns()
-  end
-  ActualSleep = (CurrTic - BegTic) / tics_per_sec
-  Diff = ActualSleep - sleep_time
-  SleepOK = true
-
-  if Diff > diff_limit
-    SleepOK = false
-    if ShowErrors
-      println("========================== sleep_ns error!! =========================================")
-      @printf("diff_limit => %12.9f secs has been exceeded!\n", diff_limit)
-      @printf("Desired => %12.9f secs  Actual => %12.9f secs  Diff => %12.9f secs\n", sleep_time, ActualSleep, Diff)
-      println("Your computer is currently slow, or 'Interrupt Timer Interval' is set too high.")
-      println("Leaving the Chrome browser open can maintain this timer at a lower level.")
-      println("See the README.md for further information.")
-      println("=====================================================================================")
-      #StoppedBecauseInterruptTimerIsSetTooHigh()  #-- undefined function to cause program stop
-      #quit()
+    while true
+      if CurrTic >= EndTic  # break out of loop
+        break
+      end
+      CurrTic = time_ns()
     end
-  end
+    ActualSleep = (CurrTic - BegTic) / TicsPerSec  #-- actual time slept
+    Diff = ActualSleep - SleepTime  #-- difference between
+    SleepOK = true
+
+    if Diff > DiffLimit
+      SleepOK = false  #-- Diff is excessibe, return false
+      if ShowErrors
+        println("========================== sleep_ns error!! =========================================")
+        @printf("DiffLimit => %12.9f secs has been exceeded!\n", DiffLimit)
+        @printf("Desired => %12.9f secs  Actual => %12.9f secs  Diff => %12.9f secs\n", SleepTime, ActualSleep, Diff)
+        println("Your computer is currently slow, or 'Interrupt Timer Interval' is set too high.")
+        println("Leaving the Chrome browser open, can maintain this timer at a lower level.")
+        println("See the README.md for further information.")
+        println("=====================================================================================")
+      end
+      if QuitOnDiffError
+        println("... Diff Error:  program halted.")
+        quit()
+      end
+    end
+  else
+    #-- there is a parm error
+    SleepOK = false
+    if QuitOnParmError
+      println("... Parm Error:  program halted.")
+      quit()
+    end
+  end  #-- ParmOk check
   return SleepOK
-end #-- end of sleep_ns
+end #-- end of sleep_ns function
 export sleep_ns
 sleep(.001)  #--warmup
 Libc.systemsleep(.001)  #--warmup
-#sleep_ns(.001)  #--warmup
 BenchmarkToolsInstalled = true
 if Pkg.installed("BenchmarkTools") !== nothing
   BenchmarkToolsInstalled = true
@@ -79,7 +97,8 @@ if Pkg.installed("BenchmarkTools") !== nothing
   include("CheckCPUImpact.jl")  #-- demo CPU utilization
   include("CheckInterruptTimer.jl")  #-- check PIC
   include("Instructions.jl")  #-- check PIC
-  include("ToyController.jl")  #-- check PIC
+  include("ToyController.jl")  #--
+  include("ThouSep.jl")  #-- insert comma separators into string
 else
   BenchmarkToolsInstalled = false
   println("\nThe package: 'BenchmarkTools' is not installed!")
